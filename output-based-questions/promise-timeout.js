da38c617c1db6a5fca7b0125cb85e21060aba134
pramoddivekar31 <68549426+pramoddivekar31@users.noreply.github.com>
2024-03-28 17:29:36 +0530

    feat/add-new questions

--- output-based-questions/promise-timeout.js
+++ output-based-questions/promise-timeout.js
@@ -1,29 +1,29 @@
-const timer = setTimeout(() => {
-  console.log("Hii");
-  setTimeout(() => {
-    console.log("Byee");
-  }, 1000);
-}, 1000);
+// const timer = setTimeout(() => {
+//   console.log("Hii");
+//   setTimeout(() => {
+//     console.log("Byee");
+//   }, 1000);
+// }, 1000);
 
-const startTime = new Date();
+// const startTime = new Date();
 
-while (true) {
-  console.log("Outside While");
-  const endTime = new Date();
-  if (endTime - startTime > 1000) {
-    console.log("Inside While");
-    clearTimeout(timer);
-    break;
-  }
-}
+// while (true) {
+//   console.log("Outside While");
+//   const endTime = new Date();
+//   if (endTime - startTime > 1000) {
+//     console.log("Inside While");
+//     clearTimeout(timer);
+//     break;
+//   }
+// }
 
-const arr = [0, 0, 0, 0];
+// const arr = [0, 0, 0, 0];
 
-for (var i = 0; i < arr.length - 1; i++) {
-  setTimeout(() => {
-    ++arr[i];
-    console.log("Arr2:", arr, arr.length, i);
-  });
-}
+// for (var i = 0; i < arr.length - 1; i++) {
+//   setTimeout(() => {
+//     ++arr[i];
+//     console.log("Arr2:", arr, arr.length, i);
+//   });
+// }
 
-console.log("Arr1:", arr);
+// console.log("Arr1:", arr);
